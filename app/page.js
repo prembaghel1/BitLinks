cc190adba39fe9deeb4aefea9a6c35a052d7aa27
prembaghel1 <prembaghell07@gmail.com>
2025-08-07 23:53:47 +0530

    pointer-cur

--- app/page.js
+++ app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
               <p className={`text-3xl font-bold ${poppins.className}`}>The best URL shortner in the market</p>
               <p className="px-28 text-center">We are the most straight forward URL shorner in the world. Most of the URL shortners will track you or ask you to give your details for login. We understand your needs. Hence we have created this URL shortner. </p>
                <div className='flex gap-3 justify-start text-white'>
-                 <Link href="/shorten"><button className='bg-purple-500 shadow-lg px-3 py-1 rounded-lg font-bold'>Try Now</button></Link>
-                 <Link href="/github"><button className='bg-purple-500 shadow-lg px-3 py-1 rounded-lg font-bold'>GitHub</button></Link>
+                 <Link href="/shorten"><button className='bg-purple-500 shadow-lg px-3 py-1 rounded-lg font-bold cursor-pointer'>Try Now</button></Link>
+                 <Link href="/github"><button className='bg-purple-500 shadow-lg px-3 py-1 rounded-lg font-bold cursor-pointer'>GitHub</button></Link>
             </div>
           </div>
           <div className="flex justify-start relative">
